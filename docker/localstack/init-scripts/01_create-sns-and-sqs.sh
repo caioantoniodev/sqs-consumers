8cd4a017bec49b38d7687d55874368aaca560712
Caio Antonio <caio.antonio.c@outlook.com>
2023-07-08 18:41:01 -0300

    first commit

---
+++ docker/localstack/init-scripts/01_create-sns-and-sqs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo "########### Create Profile ###########"
+aws configure set aws_access_key_id fakeAwsAccessKey --profile=localstack
+aws configure set aws_secret_access_key fakeAwsAcessSecret --profile=localstack
+aws configure set region us-east-1 --profile=localstack
+
+echo "########### Create SQS ###########"
+aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name queue-name.fifo --profile=localstack
+
+echo "########### Listing queues ###########"
+aws --endpoint-url=http://localhost:4566 sqs list-queues --profile=localstack
+
+echo "########### Command to view SNS messages in SQS-Subscriber ###########"
+echo "aws --endpoint-url=http://localhost:4566 sqs receive-message --queue-url http://localhost:4566/000000000000/queue-name.fifo --profile=localstack"
+
+echo "########### Command to send messages to SQS-Subscriber ###########"
+echo "aws --endpoint-url=http://localhost:4566 sqs send-message --queue-url http://localhost:4566/000000000000/queue-name.fifo --message-body teste"
